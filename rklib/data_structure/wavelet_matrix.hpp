36ea681e3a798cde60443bc5624dc2df02e86942
Ricky-pon <ricky.quiken@gmail.com>
2022-04-08 01:23:45 +0900

    separate bit_vector from wavelet_matrix

--- rklib/data_structure/wavelet_matrix.hpp
+++ rklib/data_structure/wavelet_matrix.hpp
@@ -2,41 +2,11 @@
 #define RK_WAVELET_MATRIX_HPP
 
 #include <array>
+#include <rklib/data_structure/bit_vector.hpp>
 #include <rklib/utility/utility.hpp>
 #include <vector>
 
 namespace rklib {
-
-template <class T>
-struct BitVector {
-   public:
-    BitVector() : BitVector(0) {}
-    BitVector(int n) : BitVector(std::vector<T>(n, 0), 0) {}
-    BitVector(const std::vector<T> &a, int d) {
-        int num = (a.size() >> lg) + 1;
-        sum.resize(num + 1, 0);
-        bit.resize(num, 0);
-        for (int i = 0; i < (int)a.size(); i++) {
-            if (a[i] >> d & 1) bit[i >> lg] |= (uint64_t)1 << (i & (w - 1));
-        }
-        for (int k = 0; k < num; k++) {
-            sum[k + 1] = sum[k] + __builtin_popcountll(bit[k]);
-        }
-    }
-
-    int rank(int i, int x) {
-        int k = i >> lg, l = i & (w - 1);
-        int res =
-            sum[k] + __builtin_popcountll(bit[k] & (((uint64_t)1 << l) - 1));
-        return x == 1 ? res : i - res;
-    }
-
-   private:
-    static constexpr int lg = 6;
-    static constexpr int w = 1 << lg;
-    std::vector<int> sum;
-    std::vector<uint64_t> bit;
-};
 
 template <class T, int w = 31>
 struct WaveletMatrix {
