bbe0d9a39b17c9bfe056b216914860283823b9e6
Ricky-pon <ricky.quiken@gmail.com>
2022-04-16 21:38:58 +0900

    add addition

--- rklib/data_structure/bit_set.hpp
+++ rklib/data_structure/bit_set.hpp
@@ -51,6 +51,20 @@ struct BitSet {
         }
         return *this;
     }
+    BitSet& operator+=(const BitSet& rhs) {
+        if (this->bit.size() < rhs.bit.size())
+            this->bit.resize(rhs.bit.size(), 0);
+        ulint carry = 0ULL;
+        for (size_t i = 0; i < this->bit.size() && i < rhs.bit.size(); i++) {
+            ulint tmp = 0;
+            bool flag =
+                __builtin_uaddll_overflow(this->bit[i], rhs.bit[i], &tmp);
+            flag |= __builtin_uaddll_overflow(tmp, carry, &tmp);
+            this->bit[i] = tmp;
+            carry = (flag ? 1ULL : 0ULL);
+        }
+        return *this;
+    }
     BitSet& operator<<=(const ulint& rhs) {
         if (rhs >= n) {
             std::fill(this->bit.begin(), this->bit.end(), 0ULL);
@@ -72,8 +86,23 @@ struct BitSet {
         return *this;
     }
 
+    BitSet operator~() const {
+        BitSet res(n);
+        for (size_t i = 0; i < this->bit.size(); i++) {
+            res.bit[i] = ~this->bit[i];
+        }
+        size_t j = n & mod_w;
+        if (j > 0) {
+            *res.bit.rbegin() &= mask >> (w - j);
+        }
+        return res;
+    }
+
     friend BitSet operator|(const BitSet& lhs, const BitSet& rhs) {
         return BitSet(lhs) |= rhs;
+    }
+    friend BitSet operator+(const BitSet& lhs, const BitSet& rhs) {
+        return BitSet(lhs) += rhs;
     }
     friend BitSet operator<<(const BitSet& lhs, const ulint& rhs) {
         return BitSet(lhs) <<= rhs;
