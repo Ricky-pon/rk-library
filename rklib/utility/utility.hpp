a7d28fd46853daaf671608be5a5f7632618202ba
Ricky-pon <ricky.quiken@gmail.com>
2022-04-27 23:17:41 +0900

    add chmin_non_negative

--- rklib/utility/utility.hpp
+++ rklib/utility/utility.hpp
@@ -26,6 +26,15 @@ bool chmin(T &a, const T &b) {
 }
 
 template <class T>
+bool chmin_non_negative(T &a, const T &b) {
+    if (a < 0 || a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <class T>
 T div_floor(T a, T b) {
     if (b < 0) a *= -1, b *= -1;
     return a >= 0 ? a / b : (a + 1) / b - 1;
@@ -36,32 +45,6 @@ T div_ceil(T a, T b) {
     if (b < 0) a *= -1, b *= -1;
     return a > 0 ? (a - 1) / b + 1 : a / b;
 }
-
-template <typename T>
-struct CoordComp {
-    std::vector<T> v;
-    bool sorted;
-
-    CoordComp() : sorted(false) {}
-
-    int size() { return v.size(); }
-
-    void add(T x) { v.push_back(x); }
-
-    void build() {
-        std::sort(v.begin(), v.end());
-
-        v.erase(std::unique(v.begin(), v.end()), v.end());
-        sorted = true;
-    }
-
-    int get_idx(T x) {
-        assert(sorted);
-        return lower_bound(v.begin(), v.end(), x) - v.begin();
-    }
-
-    T &operator[](int i) { return v[i]; }
-};
 
 }  // namespace rklib
 
